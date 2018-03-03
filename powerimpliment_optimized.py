5fc356756c6432a3760fed9b0c9ef81248d9342b
Gokul Gopakumar <gokulgopakumarcsengnr@gmail.com>
2018-03-03 18:48:35 +0530

    Solution uploadded in InterviewBit

---
+++ powerimpliment_optimized.py
@@ -0,0 +1,12 @@
+############################# best solution###################################
+class Solution:
+    # @param x : integer
+    # @param n : integer
+    # @param d : integer
+    # @return an integer
+    def pow(self, x, n, d):
+        if n==0:
+            return 1
+        power= self.pow(x,n/2,d)
+        return abs((x*power*power)%d) if (n &1 ) else (power*power)%d
+        
