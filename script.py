4b3ffc5c4af1791319edb8fabe3ab92258bce0d5
Gokul Gopakumar <gokulgopakumarcsengnr@gmail.com>
2018-03-05 17:50:59 +0530

    script to install packages

---
+++ script.py
@@ -0,0 +1,19 @@
+import json
+import subprocess
+from pprint import pprint
+
+with open("depends.json") as fd:
+	data = json.load(fd)
+
+	lst = []
+	for Module  in data ['Dependencies']:
+		try :
+			subprocess.check_call('sudo pip install ' + Module, shell = True)
+		except subprocess.CalledProcessError :
+ 			lst.append(Module)
+
+	if not lst:
+		print('Congrts All packages installed successfully')
+	else:
+		print('Sorry the following packages not installed, try again later')
+		pprint(lst)
